0e81fb5b58903f9759120b826d095dd666c5393a
Michelle Ranly <meranly8@gmail.com>
2020-09-23 04:00:48 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,14 @@
 class Dog 
-  def name=(dog_name)
-    @this_dogs_name = dog_name
+  def name=(name)
+    @this_dogs_name = name
   end
   
   def name
     puts @this_dogs_name
   end
   
-  def breed=(dog_breed)
-    @this_dogs_breed = dog_breed
+  def breed=(breed)
+    @this_dogs_breed = breed
   end
   
   def breed
